839a6f9db98fc592b1fb1f7d48db441b75623dd3
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-07 22:23:03 -0300

    feat: create home page and configure project

---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,99 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Input } from "@/components/ui/input"
+import CustomFormField from "../CustomFormField"
+import { Form } from "../ui/form"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneinput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton'
+}
+
+const PatientForm = () => {
+  const router = useRouter();
+  const [loading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: ""
+    },
+  })
+
+  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      const userData = { 
+        name,
+        email,
+        phone
+      };
+
+      const user = await createUntrackedSearchParams(userData);
+
+      if(user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋🏽</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="name"
+          label="Full Name"
+          placeholder="Jhon Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="email"
+          label="Email"
+          placeholder="jhondoe@jsmatery.pro"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.PHONE_INPUT}
+          name="phone"
+          label="Phone Number"
+          placeholder="(555) 123-4567"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <SubmitButton isLoading={loading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  )
+}
+
+export default PatientForm
