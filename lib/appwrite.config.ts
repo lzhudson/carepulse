ce4d7b0fe331d6e18539b4b7e880665e220056bc
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-08 11:24:41 -0300

    feat: configure appwrite

---
+++ lib/appwrite.config.ts
@@ -0,0 +1,24 @@
+import * as sdk from 'node-appwrite';
+
+const {
+  APPWRITE_PROJECT_ID,
+  APPWRITE_API_KEY,
+  APPWRITE_DATABASE_ID,
+  APPWRITE_PATIENT_COLLECTION_ID,
+  APPWRITE_DOCTOR_COLLECTION_ID,
+  APPWRITE_APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_APPWRITE_BUCKET_ID: BUCKET_ID,
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT
+} = process.env;
+
+const client = new sdk.Client();
+
+client
+  .setEndpoint(ENDPOINT!)
+  .setProject(APPWRITE_PROJECT_ID!)
+  .setKey(APPWRITE_API_KEY!);
+
+export const databases = new sdk.Databases(client);
+export const storage = new sdk.Storage(client);
+export const messagin = new sdk.Messaging(client);
+export const users = new sdk.Users(client);
